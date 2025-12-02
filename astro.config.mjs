a9e78f2efc16391a9b8183c53dbef25da609b24c
Benoit-Pierre STUDER <37024586+bpstuder@users.noreply.github.com>
2025-12-02 16:51:02 +0100

    Comment out site URL in Astro config
    The site URL in astro.config.mjs has been commented out, likely to defer setting the domain until later.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://example.com', // tu mettras ton domaine plus tard
+  //site: 'https://example.com', // tu mettras ton domaine plus tard
   integrations: [],
 });
